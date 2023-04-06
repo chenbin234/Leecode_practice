8547191047d0ce0372ca0fbc84fae69095e42c65
chenbingcheng <bingcheng.chen@outlook.com>
2023-04-06 16:14:16 +0200

    number 290

--- 290.单词规律.py
+++ 290.单词规律.py
@@ -7,12 +7,27 @@
 # @lc code=start
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s = s.split(' ')
-        alpha = []
-        for i in range(len(pattern)):
-            if pattern[i] not in alpha:
-                alpha.append(pattern[i])
-            else:
-                
+        strs = s.split(" ")
+        if len(pattern) != len(strs):
+            return False
+
+        table = {}
+        for pat, st in zip(pattern, strs):
+            # print(pat)
+            # print(st)
+            if pat not in table:
+                table[pat] = st
+            elif table[pat] != st:
+                return False
+
+        table = {}
+        for pat, st in zip(strs, pattern):
+            if pat not in table:
+                table[pat] = st
+            elif table[pat] != st:
+                return False
+
+        return True
+
 
 # @lc code=end
