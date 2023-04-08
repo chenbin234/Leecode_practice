34b1c44ef13fe2fd860c311b9ce9c266ea2039e4
chenbingcheng <bingcheng.chen@outlook.com>
2023-04-08 22:42:31 +0200

    374

---
+++ 374.猜数字大小.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=374 lang=python3
+#
+# [374] 猜数字大小
+#
+
+# @lc code=start
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        result = guess(n)
+        while True:
+            if result != 0:
+                result = guess(n)
+            else:
+                return n
+
+# @lc code=end
