916608554720a1eb0b752991dcd453e6beb39186
chenbingcheng <bingcheng.chen@outlook.com>
2023-04-11 23:21:35 +0200

    401

---
+++ 401.二进制手表.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=401 lang=python3
+#
+# [401] 二进制手表
+#
+
+# @lc code=start
+class Solution:
+    def readBinaryWatch(self, turnedOn: int):
+        result = []
+
+
+# @lc code=end
