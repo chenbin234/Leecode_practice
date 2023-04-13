4a272ff0bb7e48893560ca848785b7bae354e470
chenbingcheng <bingcheng.chen@outlook.com>
2023-04-13 18:26:55 +0200

    401

--- 401.二进制手表.py
+++ 401.二进制手表.py
@@ -8,6 +8,16 @@
 class Solution:
     def readBinaryWatch(self, turnedOn: int):
         result = []
+        hours = [1, 2, 4, 8]
+        minutes = [1,2,4,8,16,32]
+        if turnedOn > 8:
+            return result
+        if turnedOn == 0:
+            return ["0:00"]
+        for i in turnedOn+1:
+            hour_led = i
+            min_led = turnedOn-1
+            
 
 
 # @lc code=end
